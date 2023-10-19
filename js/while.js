192bd54e1c9d72cc1d8a8caab64ea1ee0db2dad8
Yadriel-Ruiz <147550067+Yadriel-Ruiz@users.noreply.github.com>
2023-10-19 16:11:57 -0500

    completed while.js

---
+++ js/while.js
@@ -0,0 +1,27 @@
+"use strict"
+
+let i = 1;
+let num = 1;
+while(i <= 16){
+    num = num * 2
+    console.log(num);
+    ++i;
+}
+
+let allCones = Math.floor(Math.random() * 50) + 50;
+let conesToSell = allCones;
+console.log(conesToSell);
+do {
+    let conesBought = Math.floor(Math.random() * 5) + 1
+    if (conesToSell > conesBought) {
+        console.log(`${conesBought} was sold`)
+        conesToSell -= conesBought;
+    }
+    else if (conesToSell < conesBought) {
+        console.log(`Cannot sell you ${conesBought} I only have ${conesToSell} left`)
+    }
+    else {
+        console.log('I sold all the cones');
+        conesToSell -= conesBought;
+    }
+}while (conesToSell > 0 )
