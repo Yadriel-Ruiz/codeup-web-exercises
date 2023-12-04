a52fdcdabc4e05383883f516fbe858fa1673dd03
Yadriel-Ruiz <147550067+Yadriel-Ruiz@users.noreply.github.com>
2023-12-04 14:37:06 -0600

    functionality of the code complete

--- js/weather_map.js
+++ js/weather_map.js
@@ -77,9 +77,8 @@ map.on('click', async function (event) {
 // Information for weather map API
 const WM_TOKEN = WM_KEY;
 function getWeatherForecast(lngLat) {
-    const lat = lngLat[1]; // Assuming latitude is at index 1
-    const lon = lngLat[0]; // Assuming longitude is at index 0
-
+    const lat = lngLat[1];
+    const lon = lngLat[0];
     const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WM_TOKEN}&units=imperial`;
 
     return fetch(url)
@@ -90,10 +89,64 @@ function getWeatherForecast(lngLat) {
             return response.json();
         })
         .then(forecastData => {
-            console.log('Weather Forecast Data:', forecastData);
-            return forecastData;
+            // Extract relevant information
+            const dailyData = {}; // Object to store daily information
+
+            forecastData.list.forEach(item => {
+                const date = item.dt_txt.split(' ')[0]; // Extract date without time
+                if (!dailyData[date]) {
+                    dailyData[date] = {
+                        minTemp: item.main.temp,
+                        maxTemp: item.main.temp,
+                        pressure: item.main.pressure,
+                        humidity: item.main.humidity,
+                        overcast: item.weather[0].description
+                    };
+                } else {
+                    // Update min/max temperature if necessary
+                    dailyData[date].minTemp = Math.min(dailyData[date].minTemp, item.main.temp);
+                    dailyData[date].maxTemp = Math.max(dailyData[date].maxTemp, item.main.temp);
+                }
+            });
+
+            console.log('Daily Weather Information:', dailyData);
+            generateWeatherCards(dailyData);
+            return dailyData;
         })
         .catch(error => {
             console.error('Error fetching weather forecast data:', error);
         });
 }
+
+function generateWeatherCards(dailyData) {
+    const container = document.getElementById('weather-cards-container');
+
+    // Clear previous cards
+    container.innerHTML = '';
+
+    // Iterate through each day in dailyData
+    for (const date in dailyData) {
+        if (dailyData.hasOwnProperty(date)) {
+            const dayInfo = dailyData[date];
+
+            // Create a card element
+            const card = document.createElement('div');
+            card.classList.add('weather-card');
+            card.classList.add('col-2');
+
+            // Update card content
+            card.innerHTML = `
+                        <h3>${date}</h3>
+                        <p>Min Temperature: ${dayInfo.minTemp} °F</p>
+                        <p>Max Temperature: ${dayInfo.maxTemp} °F</p>
+                        <p>Pressure: ${dayInfo.pressure} hPa</p>
+                        <p>Humidity: ${dayInfo.humidity}%</p>
+                        <p>Overcast: ${dayInfo.overcast}</p>
+                    `;
+
+            // Append the card to the container
+            container.appendChild(card);
+        }
+    }
+}
+
